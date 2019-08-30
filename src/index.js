28df916325e216306ae827c4396801cbe5bc0e4a
unknown <callummaguire1994@gmail.com>
2019-08-30 11:14:42 +0200

    made a spelling mistake

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import App from "../src/Componets/app";
+import App from "../src/components/app";
 import reactDOM from "react-dom";
 
 reactDOM.render(<App />, document.querySelector("#root"));
